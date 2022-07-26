3a8a65bdfb7e6e7fd1b52f0f4b2649b6e7acce69
Joshua Stephenson <josh@brightmediums.com>
2022-07-26 15:02:19 -0400

    Update to algorithm
    Guessing starts with exclusive words (words with no letters that have been matched so far) and proceeds until at least 4 letters have been matched.
    Then guessing continues with inclusive words (words that contain all letters matched so far).
    
    So, far it seems to solve in 6 steps.
    
    Next refactor: In exclusive guessing, don't guess words with duplicate letters.

--- wordle-solver.py
+++ wordle-solver.py
@@ -14,24 +14,118 @@ FRDICT = dict()
 for i in range(len(LFREQ)):
     FRDICT[LFREQ[i]] = 26-i
 
+# generate a score for words based on composition of letters and their frequency in English
+# Don't give points to duplicate letters becaues they don't help in guessing
+# Perhaps give negative value for duplicate letters
 def _get_word_points(word):
     points = 0
+    letters = []
     for letter in word:
-        points += FRDICT[letter]
+        if letter not in letters:
+            points += FRDICT[letter]
+        else:
+            points -= FRDICT[letter]
+        letters.append(letter)
     return points
 
-class Solution:
-    def __init__(self, word, guess_count):
-        self.word = word
-        self.guess_count = guess_count
 
-class Solver:
-    def __init__(self, target):
-        self.target = target
-        self.words = []
-        self._get_words()
-        self.guesses = []
+# INITIAL STATE:
+# - matches is empty
+# - unguessed is all words
+# - guessed is empty
+# - inclusive is empty (no matches)
+# - exclusive is all words
+#
+# START by making a wild guess
+# - find matching letters and add to `matches` dictionary
+# - value is position of "in position" letters or -1 for out of position letters
+# PROCEED by making an EXCLUSIVE GUESS
+# - only look for words with NO matching letters to narrow down letters faster
+# - e.g. - if letter "R" is matched, then only words without R will be guessed
+# AFTER EACH EXCLUSIVE GUESS:
+# - guess is added to `guessed` and removed from `unguessed`
+# - all words that have matching letters are added to `inclusive`
+# - all words that have matching letters are removed from `exclusive`
+# WHEN MATCHES HAS LENGTH=5, THEN SWITCH TO INCLUSIVE GUESSING
+
+class Data:
+    def __init__(self):
+        words = self._get_words()
         self.matches = dict()
+        self.unguessed  = words.copy()
+        self.guessed    = []
+        self.inclusive  = words.copy()
+        self.exclusive  = words.copy()
+
+    def add_match(self, letter, position):
+        self.matches[letter] = position
+
+    def cleanup(self):
+        # remove words from inclusive if matched letters ARE NOT in the word
+        for letter in self.matches.keys():
+            matcher = re.compile(letter)
+            inclusive = self.inclusive.copy()
+            for word in inclusive:
+                if matcher.search(word) is None:
+                    self.inclusive.remove(word)
+
+            # remove words from exclusive if matched letters ARE in the word
+            exclusive = self.exclusive.copy()
+            for word in exclusive:
+                if matcher.search(word) is not None:
+                    self.exclusive.remove(word)
+
+
+    def make_inclusive_guess(self):
+        target_count = len(self.matches)
+        guess = None
+        while guess is None:
+            if len(self.inclusive) == 0:
+                raise "No Inclusive Matches Left"
+            for word in self.inclusive:
+                current_count = 0
+                for letter in self.matches:
+                    if re.search(letter, word) is not None:
+                        current_count += 1
+                    if current_count == target_count:
+                        break
+                print("found an inclusive match: " + word)
+                guess = word
+                if word in self.inclusive:
+                    self.inclusive.remove(word)
+                self.guessed.append(word)
+                break
+        return guess
+
+    def make_exclusive_guess(self):
+        guess = None
+        while guess is None:
+            if len(self.exclusive) == 0:
+                raise "No Exclusive Matches Left"
+            for word in self.exclusive:
+                for letter in self.matches:
+                    if re.search(letter, word) is not None:
+                        # skip this word and go to next
+                        break
+                print("found an exclusive match: " + word)
+                guess = word
+                if word in self.exclusive:
+                    self.exclusive.remove(word)
+                self.guessed.append(word)
+                break
+        return guess
+
+    def next_guess(self):
+        print("Finding guess for letters: " + str(self.matches))
+
+        guess = None
+        # If we haven't matched most of the letters
+        if len(self.matches) < (LENGTH-1):
+            # make an exclusive guess
+            return self.make_exclusive_guess()
+        # otherwise, make an inclusive guess (using the letters we have matched so far)
+        return self.make_inclusive_guess()
+
 
     def _get_words(self, file=DICTIONARY):
         word_arr = []
@@ -52,58 +146,50 @@ class Solver:
 #        sorted_words = sorted(word_dict.items(), key=lambda item: item[1], reverse=True)
 #        for word in sorted_words:
 #            self.words.append(word[0])
+#
+#        print(self.words)
+        return word_arr
 
-        self.words = word_arr
+class Solution:
+    def __init__(self, word, guess_count):
+        self.word = word
+        self.guess_count = guess_count
+
+class Solver:
+    def __init__(self, target):
+        self.target = target
+        self.data = Data()
+        self.guesses = []
 
     def _is_solved(self, word):
         return word == self.target
 
-    def _find_guess(self):
-        target_count = len(self.matches)
-        guess = None
-        print("Finding guess for letters: " + str(self.matches))
-        while guess is None:
-            if len(self.words) == 0:
-                raise "No Match Found."
-            for word in self.words:
-                #print("--- " + word)
-                self.words.remove(word)
-                current_count = 0
-                for mat in self.matches:
-                    matcher = re.compile(mat)
-                    if matcher.search(word) is not None:
-                        current_count += 1
-                if current_count == target_count:
-                    print("found a possible word: " + word)
-                    guess = word
-                    self.guesses.append(guess)
-                    break
-        return guess
-
     def solve(self):
-        guess = self._find_guess()
+        guess = self.data.next_guess()
         while self._is_solved(guess) is False:
             index = 0
+
             for letter in guess:
                 matching = re.compile(letter)
                 result = matching.search(self.target)
 
-                # letter is in target word
+                # if the letter is a match
                 if result is not None:
                     position = result.span()[0]
                     # letter is in correct position
                     if position == index:
-                        self.matches[letter] = index
-
-                    # letter is not in correct position
+                        self.data.add_match(letter, position)
+                    # letter is not in the correct position
                     else:
-                        self.matches[letter] = -1
+                        self.data.add_match(letter, -1)
+                    self.data.cleanup()
+
                 index += 1
-            guess = self._find_guess()
+            guess = self.data.next_guess()
 
-        print(self.guesses)
-        return Solution(guess, len(self.guesses))
+        print(self.data.guessed)
+        return Solution(guess, len(self.data.guessed))
 
-solution = Solver("CYNIC").solve()
+solution = Solver("LYRIC").solve()
 
 print("Solved: " + solution.word + " in " + str(solution.guess_count) + " guesses.")
