f1dc55083a01690c14d0f6ed29bfb83d0581733e
Joshua Stephenson <josh@brightmediums.com>
2022-07-30 11:59:03 -0400

    Added argument parsing to make it easier to switch between running one word and all words

--- SolverTest.py
+++ SolverTest.py
@@ -1,38 +1,41 @@
 #!/Library/Frameworks/Python.framework/Versions/Current/bin/python3
 from WordleSolver import Solver
 import matplotlib.pyplot as plt
+import argparse
 
-test_one = True
-
-if test_one:
-    solution = Solver("ERROR").solve()
+parser = argparse.ArgumentParser(description='Use --all to test all wordle answers')
+parser.add_argument('-w', '--word', action="store", dest="word", help="Test one word")
+args = parser.parse_args()
+if args.word:
+    solution = Solver(args.word.upper()).solve()
     print("Solved: " + solution.word + " in " + str(solution.guess_count) + " guesses: ")
     print(solution.guesses)
+
 else:
     count = 0
     scores = dict()
     answers = dict()
     score = 0
     maximum = 0
-    with open("wordle-answers.txt", 'r') as words:
+    with open("nyt-answers.txt", 'r') as words:
         for word in words:
             count += 1
-            try:
-                solution = Solver(word.strip()).solve()
-            except TypeError:
-                print("Exception occurred here")
+#            try:
+            solution = Solver(word.strip()).solve()
+#            except BaseException:
+#                print("Exception occurred here")
+#            else:
+            score += solution.guess_count
+            if solution.guess_count > maximum:
+                maximum = solution.guess_count
+            avg = round(score / count, 4)
+            print(str(avg).ljust(6) + " " + solution.word + " " + str(solution.guess_count) + ": " + str(solution.guesses))
+            if solution.guess_count not in scores:
+                scores[solution.guess_count] = 1
+                answers[solution.guess_count] = [solution.word]
             else:
-                score += solution.guess_count
-                if solution.guess_count > maximum:
-                    maximum = solution.guess_count
-                avg = round(score / count, 4)
-                print("(" + str(avg) + " solved: " + solution.word + " in " + str(solution.guess_count) + " guesses: " + str(solution.guesses))
-                if solution.guess_count not in scores:
-                    scores[solution.guess_count] = 1
-                    answers[solution.guess_count] = [solution.word]
-                else:
-                    scores[solution.guess_count] += 1
-                    answers[solution.guess_count].append(solution.word)
+                scores[solution.guess_count] += 1
+                answers[solution.guess_count].append(solution.word)
 
     print(answers)
     print(scores)
