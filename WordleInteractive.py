d1c463067920a367f328892ea1d7ee048bb8ec26
Joshua Stephenson <josh@brightmediums.com>
2022-08-03 13:52:19 -0400

    Allowing ENTER for suggested word

--- WordleInteractive.py
+++ WordleInteractive.py
@@ -7,18 +7,20 @@ def has_duplicate_letters(word):
     return len(letters) < WORD_LENGTH
 
 solver = Solver()
-word = input('What is your first word guess? May I recommend EARST?\n> ')
+word = input('What is your first word guess? (press ENTER for EARST) \n> ')
+if len(word) < 5:
+    word = "EARST"
 is_solved = False
 while not is_solved:
     word = word.strip().upper()
     print(f'You entered: {word}')
     green = ""
     if has_duplicate_letters(word):
-        green = input('Please enter green letters in a string like \'__A__\' (or ENTER for none)\n> ')
+        green = input('Please enter green letters in a string like \'__A__\' (press ENTER for none)\n> ')
         while len(green) != 0 and len(green) != WORD_LENGTH:
             green = input("Please exactly five characters using '_' for non-green letters. Example: __A__\n> ")
     else:
-        green = input('Please enter green letters (or ENTER for none)\n> ')
+        green = input('Please enter green letters (press ENTER for none)\n> ')
         green_string = ""
         green_letters = [letter for letter in green.upper()]
         for index, letter in enumerate(word):
@@ -31,12 +33,15 @@ while not is_solved:
         break
     if len(green) == 0:
         green = None
-    yellow = input('Please enter yellow letters (or ENTER for none)\n> ')
+    yellow = input('Please enter yellow letters (press ENTER for none)\n> ')
     if len(yellow) == 0:
         yellow = None
     solver.guess(word, green, yellow)
-    print("Your next guess should be: " + solver.next_guess())
-    word = input('What is your next guess?\n> ')
+    guess = solver.next_guess()
+    print(f'Your next guess should be: {guess}')
+    word = input(f'What is your next guess? (press ENTER for {guess})\n> ')
+    if len(word) < 5:
+        word = guess
     is_solved = solver.is_solved()
 
 if is_solved:
