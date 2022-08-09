7fe90d611709e6823e328483a146d6f8b82c0676
Joshua Stephenson <josh@brightmediums.com>
2022-08-09 10:37:01 -0400

    Logging total number of words and guess count at end

--- SolverTest.py
+++ SolverTest.py
@@ -15,36 +15,35 @@ if args.rank:
 elif args.score:
     print(Dictionary().score_of(args.score))
 elif args.word:
-    solution = Solver(args.word.strip().upper()).solve()
+    solution = Solver(args.word.strip().upper()).solve("SLATE")
     print("Solved: " + solution.word + " in " + str(solution.guess_count) + " guesses: ")
     print(', '.join(solution.guesses))
 else:
-    starting_word = "EARST"
+    starting_word = "SLATE"
     dictionary = (args.dictionary if args.dictionary else "nyt-answers.txt")
     count = 0
     scores = dict()
-    score = 0
+    guess_count = 0
     maximum = 0
     hardest_words = list()
     avg = 0
-    with open(dictionary, 'r') as words:
-        for word in words:
-            count += 1
-            solution = Solver(word.strip().upper()).solve(starting_word)
-            score += solution.guess_count
-            if solution.guess_count > 6:
-                hardest_words.append(word)
-            if solution.guess_count > maximum:
-                maximum = solution.guess_count
-            avg = round(score / count, 4)
-            print(str(avg).ljust(6) + " " + solution.word + "(" + str(solution.guess_count) + "): " + str(', '.join(solution.guesses)))
+    for word in Dictionary().answers:
+        count += 1
+        solution = Solver(word).solve(starting_word)
+        guess_count += solution.guess_count
+        if solution.guess_count > 6:
+            hardest_words.append(word)
+        if solution.guess_count > maximum:
+            maximum = solution.guess_count
+        avg = round(guess_count / count, 4)
+        print(str(avg).ljust(6) + " " + solution.word + "(" + str(solution.guess_count) + "): " + str(', '.join(solution.guesses)))
 
-            if solution.guess_count not in scores:
-                scores[solution.guess_count] = {'count':1, 'words':[]}
-                scores[solution.guess_count]['words'].append(solution.word)
-            else:
-                scores[solution.guess_count]['count'] += 1
-                scores[solution.guess_count]['words'].append(solution.word)
+        if solution.guess_count not in scores:
+            scores[solution.guess_count] = {'count':1, 'words':[]}
+            scores[solution.guess_count]['words'].append(solution.word)
+        else:
+            scores[solution.guess_count]['count'] += 1
+            scores[solution.guess_count]['words'].append(solution.word)
 
     sorted_scores = dict(sorted(scores.items(), key = lambda x: x[0]))
     names = list(sorted_scores.keys())
@@ -52,6 +51,7 @@ else:
     words = list(map(lambda x: ', '.join(x[1]['words']) if x[0] > 6 else str(len(x[1]['words'])), sorted_scores.items()))
     for index, name in enumerate(names):
         print(str(name) + ": " + words[index])
+    print(f'Total Words: {count}, Total Guesses: {guess_count}')
 
 
     filename = f'results-{starting_word}-{avg}'
