a225d7cf2d05deea5d240922f1be7d621594d2d2
Joshua Stephenson <josh@brightmediums.com>
2022-08-12 17:39:27 -0400

    Adding avg check to pytest

--- test_wordle_solver.py
+++ test_wordle_solver.py
@@ -5,19 +5,20 @@ from wordle_solver import Dictionary
 # riper was a problem
 def test_riper():
     solver = Solver('RIPER')
-    solution = solver.solve('EARST')
+    solution = solver.solve('SALET')
     assert solution.guess_count <= 6
 
 def test_all():
-    starting_word = "EARST"
+    starting_word = "SLATE"
     dictionary = "nyt-answers.txt"
     count = 0
     scores = dict()
     guess_count = 0
-    maximum = 0
     hardest_words = list()
-    avg = 0
     for word in Dictionary().answers:
         count += 1
         solution = Solver(word).solve(starting_word)
+        guess_count += solution.guess_count
         assert solution.guess_count <= 6
+
+    assert round(guess_count / count, 4) < 3.68
